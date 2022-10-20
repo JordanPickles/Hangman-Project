bb8c404070039a08d7ee8f8719c16d46fbb898b3
Jordan Pickles <jordanpickles@Jordans-MacBook-Air.local>
2022-10-20 22:00:54 +0100

    Adds guess check in word

--- milestone_3.py
+++ milestone_3.py
@@ -18,3 +18,9 @@ while answer_validity == True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical characcter.")
+
+## If statement checking if guess is included in the word
+if guess in word:
+    print(f"good guess! {guess} is in the word")
+else:
+    print(f"Sorry, {guess} is not in the word. Please try again.")
